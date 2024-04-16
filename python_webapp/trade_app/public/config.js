a642cc8e3914aef9116162198d1b6a674e4ba28f
Sivaroopan Nirhoshan <nirhosh98@gmail.com>
2024-04-16 18:46:00 +0530

    Add secrets

--- python_webapp/trade_app/public/config.js
+++ python_webapp/trade_app/public/config.js
@@ -1,7 +1,7 @@
 window.configs = {
     serviceURL: '/choreo-apis/demotrial/trade/v1',
-    consumerKey: '5nyxRmWEtXU3UkpYc_MjeVCTEoIa',
-    consumerSecret: 'ntdcqRnemnvoflms2KXoWovU1uca',
+    consumerKey: 'fStHnOK2NUE2zYI6FzgPF025qwQa',
+    consumerSecret: 'lcKfgTvM3P1DFfNSIw674U8hqA8a',
     tokenURL: 'https://sts.preview-dv.choreo.dev/oauth2/token',
 };
 
