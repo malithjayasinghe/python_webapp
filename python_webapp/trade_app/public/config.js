ae92f4c511fd81ab27921e7cf2f1ab3df6fa9265
Sivaroopan Nirhoshan <nirhosh98@gmail.com>
2024-04-16 18:35:36 +0530

    add service url

--- python_webapp/trade_app/public/config.js
+++ python_webapp/trade_app/public/config.js
@@ -1,5 +1,5 @@
 window.configs = {
-    serviceURL: '/choreo-apis/demo-1/tradingservice/stock-trading-rest-endpoint-8ba/v1',
+    serviceURL: '/choreo-apis/demotrial/trade/v1',
     consumerKey: '5nyxRmWEtXU3UkpYc_MjeVCTEoIa',
     consumerSecret: 'ntdcqRnemnvoflms2KXoWovU1uca',
     tokenURL: 'https://sts.preview-dv.choreo.dev/oauth2/token',
